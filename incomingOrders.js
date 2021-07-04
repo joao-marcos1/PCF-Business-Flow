d9c2bd1457759fe38e5dcc1f9024c760d5993553
joao-marcos1 <joao.work81@gmail.com>
2021-07-04 12:48:49 +1000

    Add filter condition to the FetchXML to get only Awaiting Arrival records. Add save request of the form after editing and consequental call of verifyWeight function. Increase the height of the alertDialog a little.

--- incomingOrders.js
+++ incomingOrders.js
@@ -6,7 +6,7 @@ function onLoad(executionContext) {
     verifyWeight();
 }
 
-function onSave(executionContext) {
+async function onSave(executionContext) {
     try {
         const formContext = executionContext.getFormContext();
         const receivedWeight = formContext.getAttribute("wcs_receivedweightg").getValue();
@@ -17,22 +17,23 @@ function onSave(executionContext) {
 
         formContext.getAttribute("wcs_qcweightverified").setValue(isWeightVerified);
 
+        await formContext.data.save();
         verifyWeight();
     } catch (e) {
-        Xrm.Utility.alertDialog(e.message, { height: 120 });
+        Xrm.Utility.alertDialog(e.message, { height: 150 });
     }
 }
 
-function verifyWeight() {
+async function verifyWeight() {
     const wcs_ordernumber = GlobalFormContext.data.entity.getId();
+    const trackingStatus = 799530000;    // Awaiting Arrival tracking status
     let fetchXml = [
         "<fetch>",
           "<entity name='wcs_sampletracker'>",
             "<filter>",
               "<condition attribute='wcs_ordernumber' operator='eq' value='", wcs_ordernumber, "'/>",
-              "<filter>",
-                "<condition attribute='wcs_qcweightverified' operator='eq' value='0'/>",
-              "</filter>",
+              "<condition attribute='wcs_sampletrackingstatus' operator='eq' value='", trackingStatus, "'/>",
+              "<condition attribute='wcs_qcweightverified' operator='eq' value='0'/>",
             "</filter>",
           "</entity>",
         "</fetch>",
@@ -42,24 +43,26 @@ function verifyWeight() {
         + "/api/data/v9.0/wcs_sampletrackers?fetchXml="
         + encodeURIComponent(fetchXml);
 
-    fetch(
-        fetchUrl,
-        {
-            credentials: "same-origin",
-            headers: {
-                Prefer: 'odata.include-annotations="*"'
+    try {
+        const data = await fetch(
+            fetchUrl,
+            {
+                credentials: "same-origin",
+                headers: {
+                    Prefer: 'odata.include-annotations="*"'
+                }
             }
-        }
-    )
-    .then(response => response.json())
-    .then(data => {
+        )
+        .then(response => response.json());
+
         if (data.error) {
             throw Error(data.error.message);
         }
         // need to create column in sample tracker ??
         GlobalFormContext.getAttribute("wcs_sampleweightsverified").setValue(!data.value.length);
-    })
-    .catch(error => Xrm.Utility.alertDialog("Error: " + error.message, { height: 120 }));
+    } catch(e) {
+        Xrm.Utility.alertDialog("Error: " + e.message, { height: 150 });
+    };
 }
 
 ///Decide On Select/Save/PostSave/Load and Clean Up any ASync Issues
