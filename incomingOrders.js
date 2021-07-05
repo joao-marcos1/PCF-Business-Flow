b6c443339bfb22d096cbccae93c3ab6e7e7f58a9
joao-marcos1 <joao.work81@gmail.com>
2021-07-06 07:30:35 +1000

    Add separate function for fetch requests. Add function to get reported weight and weight allowance for the record while saving it.

--- incomingOrders.js
+++ incomingOrders.js
@@ -1,4 +1,5 @@
-GlobalFormContext;
+let GlobalFormContext;
+let isSaving = false;
 
 function onLoad(executionContext) {
     GlobalFormContext = executionContext.getFormContext();
@@ -7,40 +8,78 @@ function onLoad(executionContext) {
 }
 
 async function onSave(executionContext) {
+    if (isSaving) {
+        return;
+    }
+
+    isSaving = true;
+
+    const formContext = executionContext.getFormContext();
+
     try {
-        const formContext = executionContext.getFormContext();
-        const receivedWeight = formContext.getAttribute("wcs_receivedweightg").getValue();
-        const reportedWeight = formContext.getAttribute("wcs_reportedweightg").getValue();
-        const QCAllowance = formContext.getAttribute("wcs_qcreceivedweightallowance").getValue();
-
-        const isWeightVerified = Math.abs(reportedWeight - receivedWeight) <= Math.abs(QCAllowance);
-
-        formContext.getAttribute("wcs_qcweightverified").setValue(isWeightVerified);
-
+        await updateWeightVerified(formContext);
         await formContext.data.save();
         verifyWeight();
     } catch (e) {
         Xrm.Utility.alertDialog(e.message, { height: 150 });
     }
+    isSaving = false;
+}
+
+async function updateWeightVerified(formContext) {
+    const receivedWeight = formContext.getAttribute("wcs_receivedweightg").getValue();
+    const trackerId = formContext.data.entity.getId();
+    const { reportedWeight, weightAllowance } = await getReportedWeight(trackerId);
+    const isWeightVerified = Math.abs(reportedWeight - receivedWeight) <= Math.abs(weightAllowance);
+
+    formContext.getAttribute("wcs_qcweightverified").setValue(isWeightVerified);
+}
+
+async function getReportedWeight(wcs_sampletrackerid) {
+    const fetchXml = [
+        "<fetch>",
+            "<entity name='wcs_sampletracker'>",
+                "<attribute name='wcs_reportedweightg' />",
+                "<attribute name='wcs_qcreceivedweightallowance' />",
+                "<filter>",
+                    "<condition attribute='wcs_sampletrackerid' operator='eq' value='", wcs_sampletrackerid, "' />",
+                "</filter>",
+            "</entity>",
+        "</fetch>"
+    ].join("");
+
+    const data = await fetchData(fetchXml);
+
+    return {
+        reportedWeight: data?.value[0].wcs_reportedweightg,
+        weightAllowance: data?.value[0].wcs_qcreceivedweightallowance
+    };
 }
 
 async function verifyWeight() {
     const wcs_ordernumber = GlobalFormContext.data.entity.getId();
     const trackingStatus = 799530000;    // Awaiting Arrival tracking status
-    let fetchXml = [
+    const fetchXml = [
         "<fetch>",
-          "<entity name='wcs_sampletracker'>",
-            "<filter>",
-              "<condition attribute='wcs_ordernumber' operator='eq' value='", wcs_ordernumber, "'/>",
-              "<condition attribute='wcs_sampletrackingstatus' operator='eq' value='", trackingStatus, "'/>",
-              "<condition attribute='wcs_qcweightverified' operator='eq' value='0'/>",
-            "</filter>",
-          "</entity>",
-        "</fetch>",
+            "<entity name='wcs_sampletracker'>",
+                "<filter>",
+                    "<condition attribute='wcs_ordernumber' operator='eq' value='", wcs_ordernumber, "' />",
+                    "<condition attribute='wcs_sampletrackingstatus' operator='eq' value='", trackingStatus, "' />",
+                    "<condition attribute='wcs_qcweightverified' operator='eq' value='0' />",
+                "</filter>",
+            "</entity>",
+        "</fetch>"
     ].join("");
 
+    const data = await fetchData(fetchXml);
+    const isSampleWeightsVerified = !!data && !data.value.length;
+
+    GlobalFormContext.getAttribute("wcs_sampleweightsverified").setValue(isSampleWeightsVerified);
+}
+
+async function fetchData(fetchXml) {
     const fetchUrl = Xrm.Page.context.getClientUrl()
-        + "/api/data/v9.0/wcs_sampletrackers?fetchXml="
+        + "/api/data/v9.2/wcs_sampletrackers?fetchXml="
         + encodeURIComponent(fetchXml);
 
     try {
@@ -58,11 +97,11 @@ async function verifyWeight() {
         if (data.error) {
             throw Error(data.error.message);
         }
-        // need to create column in sample tracker ??
-        GlobalFormContext.getAttribute("wcs_sampleweightsverified").setValue(!data.value.length);
+
+        return data;
     } catch(e) {
         Xrm.Utility.alertDialog("Error: " + e.message, { height: 150 });
-    };
+    }
 }
 
 ///Decide On Select/Save/PostSave/Load and Clean Up any ASync Issues
